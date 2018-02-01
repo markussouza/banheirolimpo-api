df18dc7d35ccc44c12307981cf2f16668190c5fc
markussouza <markussouza@192.168.0.15>
2018-02-01 21:54:58 -0200

    Criação de métodos

---
+++ src/main/java/com/inova/banheirolimpo/resource/EmpresaResource.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package com.inova.banheirolimpo.resource;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.inova.banheirolimpo.model.Empresa;
+import com.inova.banheirolimpo.repository.EmpresaRepository;
+import com.inova.banheirolimpo.service.EmpresaService;
+
+/**
+ * @author Markus Souza on 01/02/2018
+ *
+ */
+@RestController
+@RequestMapping("/api/empresas")
+public class EmpresaResource {
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
+	@Autowired
+	private EmpresaService empresaService;
+	
+	public List<Empresa> obterTodos() {
+		return empresaRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Empresa> obterPorId(@PathVariable Long id) {
+		Empresa empresa = empresaRepository.findOne(id);
+		return empresa != null ? ResponseEntity.ok(empresa) : ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Empresa> criar(@Valid @RequestBody Empresa empresa) {
+		//TenantContext.setCurrentTenant(tenantName);
+		Empresa novaEmpresa = empresaRepository.save(empresa);
+		return ResponseEntity.status(HttpStatus.CREATED).body(novaEmpresa);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Empresa> atualizar(@PathVariable Long id, @Valid @RequestBody Empresa empresa) {
+		Empresa empresaAtualizada = empresaService.atualizar(id, empresa);
+		return ResponseEntity.ok(empresaAtualizada);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long id) {
+		empresaRepository.delete(id);
+	}
+
+}
--- src/main/java/com/inova/banheirolimpo/service/SensorService.java
+++ src/main/java/com/inova/banheirolimpo/service/SensorService.java
@@ -5,7 +5,9 @@ package com.inova.banheirolimpo.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.inova.banheirolimpo.model.Sensor;
@@ -23,6 +25,15 @@ public class SensorService {
 	
 	public Optional<Sensor> findByNumero(String numero) {
 		return Optional.ofNullable(sensorRepository.findByNumero(numero));
+	}
+
+	public Sensor atualizar(Long id, Sensor sensor) {
+		Sensor sensorSalvo = sensorRepository.findOne(id);
+		if (sensorSalvo == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		BeanUtils.copyProperties(sensor, sensorSalvo, "id");
+		return sensorRepository.save(sensorSalvo);
 	}
 
 }
