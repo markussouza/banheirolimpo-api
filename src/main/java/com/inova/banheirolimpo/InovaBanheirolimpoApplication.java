f0239ecdbf3576e4b4ba82e3479b2604ebddb741
Markus Souza <markus.souza@gmail.com>
2018-03-06 10:07:22 -0300

    Correção nos mapeamentos das entidades

--- src/main/java/com/inova/banheirolimpo/InovaBanheirolimpoApplication.java
+++ src/main/java/com/inova/banheirolimpo/InovaBanheirolimpoApplication.java
@@ -4,12 +4,14 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableScheduling
 public class InovaBanheirolimpoApplication {
 
